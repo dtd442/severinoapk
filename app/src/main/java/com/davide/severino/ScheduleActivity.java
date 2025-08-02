121cc3ed92c140c8a0c910bffe9fe912591bb25c
root <root@localhost>
2025-08-02 11:36:39 +0000

    fix:wf

---
+++ app/src/main/java/com/davide/severino/ScheduleActivity.java
@@ -0,0 +1,18 @@
+package com.davide.severino;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScheduleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("📅 ScheduleActivity - Coming soon...");
+        tv.setTextSize(20);
+        tv.setPadding(50, 100, 50, 50);
+        setContentView(tv);
+    }
+}
